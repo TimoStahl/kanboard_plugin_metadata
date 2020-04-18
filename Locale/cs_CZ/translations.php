393648338d588e85ef72f62d9b64a01274b1746e
trendspotter <j.podhorecky@volny.cz>
2020-04-18 17:44:10 +0200

    Add cs_CZ translations

---
+++ Locale/cs_CZ/translations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Metadata'                                    => 'Metadata',
+    'Key'                                         => 'Klíč',
+    'Value'                                       => 'Hodnota',
+    'No metadata'                                 => 'Žádná metadata',
+    'Remove Metadata'                             => 'Odebrat metadata',
+    'Do you really want to remove this metadata?' => 'Opravdu chcete odebrat tato metadata?',
+    'Add Metadata'                                => 'Přidat metadata',
+];
